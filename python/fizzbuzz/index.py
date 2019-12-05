77351baec156cf839cf866765928847d1c7210e3
Rahul Patil <rahul.patil@c3m.io>
2019-12-05 12:53:12 +0530

    fizzbuzz

---
+++ python/fizzbuzz/index.py
@@ -0,0 +1,25 @@
+# Write a program that console logs the numbers
+# from 1 to n. But for multiples of three print
+# 'fizz' instead of the number and for the multiples
+# of five print 'buzz'. For numbers which are multiples
+# of both three and five print 'fizzbuzz'.
+#  Example
+#   fizzBuzz(5);
+#   1
+#   2
+#   fizz
+#   4
+#   buzz
+
+def fizzBuzz(n):
+    for i in range(1, n+1):
+        if(i%3==0 and i%5==0):
+            print("fizzbuzz")
+        elif(i%3==0):
+            print("fizz")
+        elif(i%5==0):
+            print("buzz")
+        else:
+            print(i)
+
+fizzBuzz(15)
